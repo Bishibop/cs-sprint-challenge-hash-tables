1166cbf5ca9989b850a6d0a5c242b32105fdca7f
Nicholas Mullen <nwmullen@gmail.com>
2020-09-11 22:16:50 -0400

    ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,25 +1,23 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    file_to_paths = {}
+
+    for file_path in files:
+        file_name = file_path.split('/')[-1]
+        if file_name in file_to_paths:
+            file_to_paths[file_name].append(file_path)
+        else:
+            file_to_paths[file_name] = [file_path]
+
+    result = []
+    for query in queries:
+        if query in file_to_paths:
+            result.extend(file_to_paths[query])
 
     return result
 
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
+    files = ['/bin/foo', '/bin/bar', '/usr/bin/baz']
+    queries = ["foo", "qux", "baz"]
     print(finder(files, queries))
